56593eb3716126a29d39fdbb81038dd385bd8f07
Raquel Illerias <raquel.illerias@gmail.com>
2024-08-22 14:25:16 +0100

    add styling to header image and sign up button

--- app/screens/HomeScreen.js
+++ app/screens/HomeScreen.js
@@ -1,7 +1,7 @@
-import { Text, View, StyleSheet, ScrollView, Image, Button, SafeAreaView } from 'react-native';
+import { Text, View, StyleSheet, ScrollView, Image, Button, SafeAreaView, TouchableOpacity } from 'react-native';
 
 import colours from '../config/colours';
-import { width } from '@fortawesome/free-solid-svg-icons/fa0';
+import { height, width } from '@fortawesome/free-solid-svg-icons/fa0';
 
 function HomeScreen({navigation}) {
     return (
@@ -9,7 +9,7 @@ function HomeScreen({navigation}) {
             <ScrollView >
                 <View style={styles.headerContainer}>
                     <Image
-                      style={styles.image}
+                      style={styles.headerImg}
                       source={require("../assets/busk-a-move-header.png")}
                     />
                 </View>
@@ -18,9 +18,12 @@ function HomeScreen({navigation}) {
                     <Image style={styles.image} source={{uri: "https://images.unsplash.com/photo-1483069125343-4ef290c07840?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}}/>
                 </View>           
                 <Text style={styles.blurb}>Welcome, it seems you've stumbled upon the Busk-A-Move app, a collaboration app for busking musicians at any level. Whether you've never busked before and want to get started, or you're a veteran Busker wanting to spread your music upon fellow musicians and passers-by, this app can help organise your busks and collaborate with local Buskers, get started by making an account, then you can peruse Busks created by other Buskers, check the Buskers out, or start your Busking journey by creating your own Busks. Happy Busking!!!</Text>
-            <Button title="Sign up" onPress={() => {
-                navigation.navigate("SignUp")
-            }}/>
+                <TouchableOpacity style={styles.signUpButton} onPress={() =>
+                {
+                    navigation.navigate("SignUp")
+                }}>
+                    <Text style={styles.signUpText}>SIGN UP</Text>
+                </TouchableOpacity>
             </ScrollView>
         </SafeAreaView>
     );
@@ -30,6 +33,8 @@ export default HomeScreen;
 
 const styles = StyleSheet.create({
     container: {
+        padding: 20,
+        paddingTop: 30,
         flex: 1,
         backgroundColor: colours.secondaryBackground,
     },
@@ -41,9 +46,13 @@ const styles = StyleSheet.create({
     },
     headerContainer: {
         width: "100%",
+        height: 90,
         display: "flex",
-        backgroundColor: "red",
-
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    headerImg: {
+        width: 280
     },
     text: {
         // fontSize: 40,
@@ -62,7 +71,18 @@ const styles = StyleSheet.create({
     blurb: {
         textAlign: "justify",
         color: colours.primaryHighlight,
-        marginHorizontal: 50,
+        marginHorizontal: 35,
+        marginVertical: 20,
         // fontSize: 20
-    }
+    },
+    signUpButton: {
+        backgroundColor: colours.primaryHighlight,
+        padding: 15,
+        alignItems: 'center',
+        borderRadius: 5,
+        marginVertical: 20,
+    },
+    signUpText: {
+        color: colours.lightText,
+  },
 });
