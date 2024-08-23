42dd31d9273ac374425bef0db6762e5eb1e1c6ca
BassManLuke <luke_haz@yahoo.co.uk>
2024-08-23 15:14:10 +0100

    install react native picker select

--- app/components/BuskSearchComponent.js
+++ app/components/BuskSearchComponent.js
@@ -1,13 +1,25 @@
-import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-function BuskSearchComponent(props) {
+function BuskSearchComponent() {
     return (
-        <View>
-            <Text>
+        <View style={styles.filterContainer}>
+            <Text >
                 BuskSearchComponent
             </Text>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		// backgroundColor: "#f5f5f5",
+		paddingTop: StatusBar.currentHeight,
+	},
+    filterContainer: {
+        color: "red"
+    }
+});
 
 export default BuskSearchComponent;
