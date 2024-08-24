c887a1025574388089663d27b5fda0252fd48683
BassManLuke <luke_haz@yahoo.co.uk>
2024-08-24 21:23:26 +0100

    update Busk List with more relevant data and add styling

--- app/stacks/HomeStack.js
+++ app/stacks/HomeStack.js
@@ -2,14 +2,26 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
 import SignupScreen from '../screens/SignupScreen';
 import BusksScreen from '../screens/BusksScreen';
+import colours from '../config/colours';
 
 const Stack = createNativeStackNavigator()
 
 function ChatsStack() {
     return (
         <Stack.Navigator>
-            <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headerShown: false}} />
-            <Stack.Screen name="SignUp" component={SignupScreen} />
+            <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headerShown: false, title: "Home"}} />
+            <Stack.Screen 
+                name="SignUp" 
+                component={SignupScreen} 
+                options={{
+                    title: "Sign Up", 
+                    headerTitleStyle: {
+                        fontWeight: "bold"
+                    }, 
+                    headerStyle: {
+                        backgroundColor: colours.secondaryBackground
+                    }
+                }}/>
             <Stack.Screen name="Busks" component={BusksScreen} />
         </Stack.Navigator>
     );
