ce4da386d6f351223032b94d4265f62b06624abd
moricode49 <samhurley4@hotmail.com>
2024-08-28 13:30:46 +0100

    update users location on busks screen

--- app/screens/BusksScreen.js
+++ app/screens/BusksScreen.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import {
 	Text,
 	View,
@@ -10,22 +10,21 @@ import {
 	TouchableWithoutFeedback,
 	Image,
 	ScrollView,
-} from 'react-native';
-import { fetchAllBusks } from '../api';
-import { PROVIDER_GOOGLE } from 'react-native-maps';
-import MapView from 'react-native-maps';
-import { Marker } from 'react-native-maps';
-import BuskSearchComponent from '../components/BuskSearchComponent';
-import colours from '../config/colours';
-import { formatDate, formatTime } from '../assets/utils/date-and-time';
-import * as Location from 'expo-location';
+} from "react-native";
+import { fetchAllBusks } from "../api";
+import { PROVIDER_GOOGLE } from "react-native-maps";
+import MapView from "react-native-maps";
+import { Marker } from "react-native-maps";
+import BuskSearchComponent from "../components/BuskSearchComponent";
+import colours from "../config/colours";
+import { formatDate, formatTime } from "../assets/utils/date-and-time";
+import * as Location from "expo-location";
 
 function BusksScreen({ navigation }) {
-	const [currentLocation, setCurrentLocation] = useState(null);
 	const [initialRegion, setInitialRegion] = useState(null);
 	const [busksList, setBusksList] = useState([]);
-	const [sortBy, setSortBy] = useState('');
-	const [instrumentFilter, setInstrumentFilter] = useState('');
+	const [sortBy, setSortBy] = useState("");
+	const [instrumentFilter, setInstrumentFilter] = useState("");
 	const [isLoading, setIsLoading] = useState(true);
 	const [refreshing, setRefreshing] = useState(false);
 	const [mapLocations, setMapLocations] = useState([]);
@@ -47,30 +46,31 @@ function BusksScreen({ navigation }) {
 			});
 			setMapLocations(listOfMapLocations);
 			setIsLoading(false);
+			const getLocation = async () => {
+				let { status } = await Location.requestForegroundPermissionsAsync();
+				if (status !== "granted") {
+					console.log("Permission to access location was denied");
+					return;
+				}
+				let location = await Location.getCurrentPositionAsync({});
+				// setCurrentLocation(location.coords);
+				return location.coords;
+			};
+			getLocation().then((response) => {
+				setInitialRegion({
+					latitude: response.latitude,
+					longitude: response.longitude,
+					latitudeDelta: 0.0422,
+					longitudeDelta: 0.0221,
+				});
+			});
 		});
-		const getLocation = async () => {
-			let { status } = await Location.requestForegroundPermissionsAsync();
-			if (status !== 'granted') {
-				console.log('Permission to access location was denied');
-				return;
-			}
-			let location = await Location.getCurrentPositionAsync({});
-			setCurrentLocation(location.coords);
-			//
-		};
-		getLocation();
-		// setInitialRegion({
-		// 	latitude: response.busk_location.latitude,
-		// 	longitude: response.busks.busk_location.longitude,
-		// 	latitudeDelta: 0.0422,
-		// 	longitudeDelta: 0.0221,
-		// });
 	}, [instrumentFilter, sortBy]);
 
 	if (isLoading) {
 		return (
 			<SafeAreaView style={styles.loadingContainer}>
-				<ActivityIndicator size='large' color='0000ff' />
+				<ActivityIndicator size="large" color="0000ff" />
 				<Text>Loading...</Text>
 			</SafeAreaView>
 		);
@@ -90,42 +90,44 @@ function BusksScreen({ navigation }) {
 							/>
 						</View>
 						<View style={styles.mapContainer}>
-							<MapView
-								style={styles.map}
-								provider={PROVIDER_GOOGLE}
-								showsUserLocation
-								initialRegion={{
-									latitude: Number(currentLocation.latitude),
-									longitude: Number(currentLocation.longitude),
-									latitudeDelta: 0.0902,
-									longitudeDelta: 0.0901,
-								}}
-							>
-								{mapLocations.map((marker, index) => {
-									return (
-										<Marker
-											key={index}
-											coordinate={{
-												latitude: marker.latitude,
-												longitude: marker.longitude,
-											}}
-											title={marker.locationName}
-											description='Busk location'
-										/>
-									);
-								})}
-							</MapView>
+							{initialRegion && (
+								<MapView
+									style={styles.map}
+									provider={PROVIDER_GOOGLE}
+									showsUserLocation
+									initialRegion={{
+										latitude: Number(initialRegion.latitude),
+										longitude: Number(initialRegion.longitude),
+										latitudeDelta: 0.0902,
+										longitudeDelta: 0.0901,
+									}}
+								>
+									{mapLocations.map((marker, index) => {
+										return (
+											<Marker
+												key={index}
+												coordinate={{
+													latitude: marker.latitude,
+													longitude: marker.longitude,
+												}}
+												title={marker.locationName}
+												description="Busk location"
+											/>
+										);
+									})}
+								</MapView>
+							)}
 						</View>
 					</View>
 				}
 				data={busksList}
 				renderItem={({ item }) => {
-					const instruments = item.busk_selected_instruments.join(', ');
+					const instruments = item.busk_selected_instruments.join(", ");
 
 					return (
 						<TouchableWithoutFeedback
 							onPress={() => {
-								navigation.navigate('SingleBusk', { id: item.busk_id });
+								navigation.navigate("SingleBusk", { id: item.busk_id });
 							}}
 						>
 							<View style={styles.card}>
@@ -134,8 +136,8 @@ function BusksScreen({ navigation }) {
 									source={{ uri: item.user_image_url }}
 								/>
 								<Text style={styles.titleText}>
-									{item.username} @ {item.busk_location_name} @{' '}
-									{formatTime(item.busk_time_date)} on{' '}
+									{item.username} @ {item.busk_location_name} @{" "}
+									{formatTime(item.busk_time_date)} on{" "}
 									{formatDate(item.busk_time_date)}
 								</Text>
 								<View style={styles.bodyContainer}>
@@ -166,17 +168,17 @@ function BusksScreen({ navigation }) {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#f5f5f5',
+		backgroundColor: "#f5f5f5",
 		paddingTop: StatusBar.currentHeight,
-		flexDirection: 'column',
-		alignItems: 'center',
+		flexDirection: "column",
+		alignItems: "center",
 	},
 	filterContainer: {
-		flexDirection: 'column',
-		alignItems: 'center',
+		flexDirection: "column",
+		alignItems: "center",
 	},
 	card: {
-		width: '95%',
+		width: "95%",
 		aspectRatio: 1 / 1,
 		backgroundColor: colours.secondaryBackground,
 		padding: 16,
@@ -187,9 +189,9 @@ const styles = StyleSheet.create({
 	cardImage: {
 		margin: 16,
 		opacity: 0.2,
-		width: '100%',
+		width: "100%",
 		aspectRatio: 1 / 1,
-		position: 'absolute',
+		position: "absolute",
 	},
 	titleText: {
 		fontSize: 25,
@@ -197,37 +199,37 @@ const styles = StyleSheet.create({
 	bodyText: {
 		fontSize: 20,
 		color: colours.darkHighlight,
-		textAlign: 'right',
+		textAlign: "right",
 	},
 	bodyContainer: {
 		margin: 16,
-		width: '100%',
+		width: "100%",
 		aspectRatio: 1 / 1,
-		position: 'absolute',
-		alignItems: 'flex-end',
-		justifyContent: 'flex-end',
+		position: "absolute",
+		alignItems: "flex-end",
+		justifyContent: "flex-end",
 	},
 	headerText: {
 		fontSize: 24,
-		textAlign: 'center',
+		textAlign: "center",
 		marginBottom: 12,
 		paddingTop: 10,
 	},
 	footerText: {
 		fontSize: 24,
-		textAlign: 'center',
+		textAlign: "center",
 		marginTop: 12,
 	},
 	loadingContainer: {
 		flex: 1,
-		backgroundColor: 'F5F5F5',
+		backgroundColor: "F5F5F5",
 		paddingTop: StatusBar.currentHeight,
-		justifyContent: 'center',
-		alignItems: 'center',
+		justifyContent: "center",
+		alignItems: "center",
 	},
 	mapContainer: {
-		flexDirection: 'column',
-		alignItems: 'center',
+		flexDirection: "column",
+		alignItems: "center",
 		margin: 10,
 		border: colours.darkTabBar,
 		borderWidth: 5,
