e55b4dcd4579792fae7bd8fb64973b1bf54f3a69
moricode49 <samhurley4@hotmail.com>
2024-08-29 10:34:55 +0100

    styling myprofile page and added create busk button

--- app/stacks/UserStack.js
+++ app/stacks/UserStack.js
@@ -1,47 +1,63 @@
-import { Text, View, ActivityIndicator } from 'react-native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from '../screens/LoginScreen';
-import SignupScreen from '../screens/SignupScreen';
-import MyProfileScreen from '../screens/MyProfileScreen';
-import {useState, useEffect} from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useNavigation } from '@react-navigation/native';
+import { Text, View, ActivityIndicator } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "../screens/LoginScreen";
+import SignupScreen from "../screens/SignupScreen";
+import MyProfileScreen from "../screens/MyProfileScreen";
+import { useState, useEffect } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useNavigation } from "@react-navigation/native";
+import CreateABuskScreen from "../screens/CreateABuskScreen";
+import colours from "../config/colours";
 
-const Stack = createNativeStackNavigator()
+const Stack = createNativeStackNavigator();
 
-function UserStack({navigation}) {
-    const [isAuthenticated, setIsAuthenticated] = useState(false)
-    const [isLoading, setIsLoading] = useState(true)
+function UserStack({ navigation }) {
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+	const [isLoading, setIsLoading] = useState(true);
 
-const nav = useNavigation()
+	const nav = useNavigation();
 
-    useEffect(() => {
-        AsyncStorage.getItem('isAuthenticated').then((flag) =>  {
-            if (flag === 'true') {
-                setIsAuthenticated(true)
-                // nav.reset({index: 0,
-                //     routes:[{name: 'MyProfile'}]
-                // })
-            }setIsLoading(false)
-        })
-    }, [])
+	useEffect(() => {
+		AsyncStorage.getItem("isAuthenticated").then((flag) => {
+			if (flag === "true") {
+				setIsAuthenticated(true);
+				// nav.reset({index: 0,
+				//     routes:[{name: 'MyProfile'}]
+				// })
+			}
+			setIsLoading(false);
+		});
+	}, []);
 
-    if (isLoading) {
-        return (
-          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <ActivityIndicator size="large" color="#008b8b" />
-          </View>
-        );
-      }
+	if (isLoading) {
+		return (
+			<View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+				<ActivityIndicator size="large" color="#008b8b" />
+			</View>
+		);
+	}
 
-    return (
-        <Stack.Navigator initialRouteName={isAuthenticated ? 'Myprofile' : 'Login'}>
-            
-            <Stack.Screen name="Login" component={LoginScreen}/>
-            <Stack.Screen name="SignUp" component={SignupScreen}/>
-            <Stack.Screen name="MyProfile" component={MyProfileScreen}/>
-        </Stack.Navigator>
-    );
+	return (
+		<Stack.Navigator initialRouteName={isAuthenticated ? "Myprofile" : "Login"}>
+			<Stack.Screen name="Login" component={LoginScreen} />
+			<Stack.Screen name="SignUp" component={SignupScreen} />
+			<Stack.Screen name="MyProfile" component={MyProfileScreen} />
+			<Stack.Screen
+				name="CreateABusk"
+				component={CreateABuskScreen}
+				options={{
+					title: "Create New Busk",
+					headerTitleStyle: {
+						fontWeight: "bold",
+						fontSize: 23,
+					},
+					headerStyle: {
+						backgroundColor: colours.secondaryBackground,
+					},
+				}}
+			/>
+		</Stack.Navigator>
+	);
 }
 
 export default UserStack;
